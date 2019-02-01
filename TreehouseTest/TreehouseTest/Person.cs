0d68b4b8f8477a652342d6ac1b4f1ce3b2b21c91
dmayers <dmayers@DEVARCH>
2019-02-01 08:10:20 -0600

    Split name into First and Last name.

--- TreehouseTest/TreehouseTest/Person.cs
+++ TreehouseTest/TreehouseTest/Person.cs
@@ -8,7 +8,10 @@ namespace TreehouseTest
 {
     class Person
     {
-        public string Name { get; set; }
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
         public string Address { get; set; }
     }
 }
